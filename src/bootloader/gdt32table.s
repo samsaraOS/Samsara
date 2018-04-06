0ffff9fd3cf7922b7a30318e52bfdaaef78f1e5e
Dani Frisk <dani@silverskin.fi>
2018-04-06 10:44:19 +0300

    Update Global Descriptor Table name

---
+++ src/bootloader/gdt32table.s
@@ -0,0 +1,31 @@
+;***
+; In this file, we'll define GDT
+;***
+%ifndef GDT_HEADER
+
+GDT_PTR:
+	dw 	GDT_END - GDT_START - 1
+	dd 	GDT_START
+
+GDT_START:
+.null:
+	dd 	0
+	dd 	0
+.code:
+	dw 	0xFFFF
+	dw 	0
+	db 	0
+	db 	10011010b
+	db 	11001111b
+	db 	0
+.data:
+	dw 	0xFFFF
+	dw 	0
+	db 	0
+	db 	10010010b
+	db 	11001111b
+	db 	0
+GDT_END:
+
+%endif
+
