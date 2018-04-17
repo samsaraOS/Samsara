31cbe388c37a92c7b2323e3911044f75ac55331d
Arash T.C <Captainarash@users.noreply.github.com>
2018-04-17 15:41:12 +0300

    Fixed errors
    forgot the int vector before.
    now it's fixed
    also pop instead of push ;D

--- src/kernel/arch/x86/desc_tables/interrupt_handler.s
+++ src/kernel/arch/x86/desc_tables/interrupt_handler.s
@@ -33,20 +33,21 @@ interrupt_handler:
         call   interrupt_dispatcher
 
         pop     rflags
-        push    r15
-        push    r14
-        push    r13
-        push    r12
-        push    r11
-        push    r10
-        push    r9
-        push    r8
-        push    rdi
-        push    rsi
-        push    rdx
-        push    rcx
-        push    rbx
-        push    rax
+        pop     r15
+        pop     r14
+        pop     r13
+        pop     r12
+        pop     r11
+        pop     r10
+        pop     r9
+        pop     r8
+        pop     rdi
+        pop     rsi
+        pop     rdx
+        pop     rcx
+        pop     rbx
+        pop     rax
+        add     rsp,0x8 ;* we don't forget the interrupt vector :D 
         iret
 
 
