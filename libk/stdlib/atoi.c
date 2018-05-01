bf97e61efca7745d5f03a9c96c1042892466a438
Arash T.C <Captainarash@users.noreply.github.com>
2018-05-01 18:58:38 +0300

    Fixed atoi.c
    1) Fixed a minor bug.
    2) Added Sanity check.

--- libk/stdlib/atoi.c
+++ libk/stdlib/atoi.c
@@ -6,15 +6,17 @@ atoi(char *str)
 {
 	int ret;
 	int i;
-	
+
 	ret = 0;
        	i = 0;
 
 	do {
-		ret = (ret * 10 + str[i] - '\0');
+		/* if the character is not a number, we'll break the loop */
+		if (str[i] > '9' && str[i] < '0') {
+			break;
+		}
+		ret = (ret * 10 + str[i] - '0');
 	} while (i++ && str[i]);
-	
+
 	return ret;
 }
-
-
