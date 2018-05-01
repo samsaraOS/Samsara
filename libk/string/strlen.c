0f726bdc71b1ae6870ddda8a27d633dba9be08a8
Arash T.C <Captainarash@users.noreply.github.com>
2018-05-01 19:30:01 +0300

    Fixed bugs and minor updates
    1) Fixed an infinite loop bug
    2) Set a MAX_LENGTH for strlen
    3) Added Comments
    4) IO_LOCK must be implemented soon

--- libk/string/strlen.c
+++ libk/string/strlen.c
@@ -1,21 +1,25 @@
 
 #include <string.h>
 
+/*	we check for the maximum length of a string which currently is a
+	random number since one can't allocate the entire address space to
+	a string anyway.
+	We set the maximum to a thousand.
+	IO_Lock function will be implemented later.
+*/
+#define MAX_LENGTH 1000
+
 size_t
 strlen(const char *str)
 {
 	size_t i;
 	i = 0;
 	do {
-		if ((i + 1) < 1 ||
-		    (i > 0xFFFFFFFE))
+		i++;
+		if (i > MAX_LENGTH)
 		{
 			break;
 		}
 	} while (str[i] != 0x00);
 	return i;
 }
-
-
-
-
